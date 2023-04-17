721dd4f23b86cbf321c242e33ecccd9d32102feb
infosys <infosys@C4217>
2023-04-17 10:11:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample/ConsoleApp0411/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0411 {
+    class Program {
+        static void Main(string[] args) {
+            string[] moneyString = { "一万円札", "五千円札", "二千円札", "千円札",
+                                        "五百円玉", "百円玉", "五十円玉", "十円玉", "五円玉", "一円玉" };
+            int[] moneyInteger = { 10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1 };
+
+            //金額の入力
+            Console.Write("合計金額：");
+            int total = int.Parse(Console.ReadLine());
+
+            //支払い金額入力
+            Console.Write("支払い：");
+            int pay = int.Parse(Console.ReadLine());
+
+            //お釣りの計算
+            int change = pay - total;
+            Console.WriteLine("お釣り：{0}円", change);
+
+            //金種枚数出力
+            for (int i = 0; i < moneyString.Length; i++)
+            {
+                Console.WriteLine(moneyString[i] + "：{0}枚", change / moneyInteger[i]);
+                change %= moneyInteger[i];
+            }
+
+        }
+    }
+}
---
+++ Sample/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btButton = new System.Windows.Forms.Button();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.textAns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btButton
+            // 
+            this.btButton.Location = new System.Drawing.Point(217, 84);
+            this.btButton.Name = "btButton";
+            this.btButton.Size = new System.Drawing.Size(151, 205);
+            this.btButton.TabIndex = 0;
+            this.btButton.Text = "押せ！";
+            this.btButton.UseVisualStyleBackColor = true;
+            this.btButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.BackColor = System.Drawing.Color.OrangeRed;
+            this.tbNum1.Font = new System.Drawing.Font("Magneto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum1.Location = new System.Drawing.Point(12, 24);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(151, 22);
+            this.tbNum1.TabIndex = 1;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.BackColor = System.Drawing.Color.OrangeRed;
+            this.tbNum2.Font = new System.Drawing.Font("Magneto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNum2.Location = new System.Drawing.Point(217, 24);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(151, 22);
+            this.tbNum2.TabIndex = 1;
+            // 
+            // textAns
+            // 
+            this.textAns.BackColor = System.Drawing.Color.OrangeRed;
+            this.textAns.Font = new System.Drawing.Font("Magneto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textAns.Location = new System.Drawing.Point(434, 24);
+            this.textAns.Name = "textAns";
+            this.textAns.Size = new System.Drawing.Size(151, 22);
+            this.textAns.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Onyx", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Onyx", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(627, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textAns);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.btButton);
+            this.Font = new System.Drawing.Font("Onyx", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "Form1";
+            this.Text = "B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btButton;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox textAns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
