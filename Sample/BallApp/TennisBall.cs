7b32bb7d02dc314920258d686d80758a61b531ef
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-04-26 11:28:25 +0900

    abstract

--- Sample/BallApp/TennisBall.cs
+++ Sample/BallApp/TennisBall.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BallApp {
     class TennisBall : Obj {
@@ -34,6 +35,10 @@ namespace BallApp {
 
             PosX += MoveX;
             PosY += MoveY;
+        }
+        public override void Move(Keys direction) {
+
+
         }
     }
 }
