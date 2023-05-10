8e6869787ccc473f170ea8e0e6e4823cf1c6e769
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-10 11:25:20 +0900

    距離換算プログラム完成

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -8,13 +8,17 @@ namespace DistanceConverter {
     //フィートとメートルの単位変換クラス
     public static class FeetConverter {
 
+        /// </summary>定数
+        private const double ratio = 0.3048;
+
+
         //フィートからメートルを求める
         public static double FromMeter(int feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
         //メートルからフィートを求める
         public static　double ToMeter(int meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
     }
 }
