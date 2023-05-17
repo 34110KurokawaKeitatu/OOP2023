dabc914ce65486505b1f4ef1172af64ff9302e48
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-17 11:22:30 +0900

    p79 問題.21完成

---
+++ Chapter02/演習問題/Ecossaises01/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecossaises01 {
+    public class Song {
+
+        public String Title { get; set; }//歌手タイトル
+        public String ArtistName { get; set; }// アーティストタイトル
+        public int Length { get; set; }//演奏時間
+
+        //2.1.2
+        public Song(String title, String artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
