d380d61f166c8def7d395f1a03eda6f3632ad38b
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-17 12:09:48 +0900

    結果にコミット　ライザップ

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //フィートとメートルの単位変換クラス
+    public static class InchConverter {
+
+        /// </summary>定数
+        private const double ratio = 0.0254;
+
+
+        //フィートからメートルを求める
+        public static double FromMeter(int feet) {
+            return feet * ratio;
+        }
+        //メートルからフィートを求める
+        public static　double ToMeter(int meter) {
+            return meter / ratio;
+        }
+    }
+}
