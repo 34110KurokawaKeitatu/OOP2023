36c8f2080c6b38745cb466f5011f025031627eaa
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-22 11:21:18 +0900

    結果

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Count(5));
+        }
+        static public int Count (int num) {
+            var numbers = new[] { 5, 3, 9, 7, 7, 5, 8, 1, 0, 5, 10, 4 };
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (num == numbers[i])
+                {
+                    count++;
+                }
+                
+            }
+            return count;
+        }
+    }
+}
