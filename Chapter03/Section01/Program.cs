75e3175726046231863cd9ba0ac3492dd6b79e92
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-22 11:58:20 +0900

    ラムダ式以前　（デリゲート、匿名メソッド）

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -6,19 +6,28 @@ using System.Threading.Tasks;
 
 namespace Section01 {
     class Program {
+
+       // public delegate bool Judgement(int value); //ゲリゲートの宣言
+
         static void Main(string[] args) {
-            Console.WriteLine(Count(5));
-        }
-        static public int Count (int num) {
             var numbers = new[] { 5, 3, 9, 7, 7, 5, 8, 1, 0, 5, 10, 4 };
+            // Judgement judge = IsEven;
+            int count = Count(numbers, delegate (int n) { return n % 2 == 0; });
+            Console.WriteLine(count);
+        }
+
+        static public bool IsEven(int n) {
+            return n % 2 == 1;
+        }
+
+        static public int Count (int[] numbers ,Predicate<int> judge) {
             int count = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            foreach (var n in numbers)
             {
-                if (num == numbers[i])
+                if (judge(n) == true)
                 {
                     count++;
                 }
-                
             }
             return count;
         }
