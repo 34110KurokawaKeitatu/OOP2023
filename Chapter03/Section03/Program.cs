476bb4ae0b767461728e6a2b805641627cebc455
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-23 12:09:00 +0900

    第3章の演習問題1

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,18 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[0] == 'k');
+            //Console.WriteLine(exists);
+
+            //var name = list.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
+
+            //var index = list.FindIndex(s => s == "Canderra");
+            //Console.WriteLine(index);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
         }
     }
 }
