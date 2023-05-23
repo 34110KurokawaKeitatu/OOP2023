711590c61442cef4622fb5675f3b8fffddcd7646
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-05-23 10:31:09 +0900

    p87　結果にコミットライザップ

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -12,8 +12,11 @@ namespace Section01 {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 7, 7, 5, 8, 1, 0, 5, 10, 4 };
             // Judgement judge = IsEven;
-            int count = numbers.Count(n => n % 2 == 0);
-            Console.WriteLine(count);
+
+
+            //int count = numbers.Count(n => n % 5 == 0 && n != 0);
+            var sum = numbers.Where(n=>n%2==0).Average();
+            Console.WriteLine(sum);
         }
 
     }
