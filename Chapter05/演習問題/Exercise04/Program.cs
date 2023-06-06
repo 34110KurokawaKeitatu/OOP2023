7d2185a2962f6e56d460446f71cb1205d068f9d4
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-06-06 10:37:03 +0900

    配列

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,14 +8,32 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var repl = line.Replace("Novelist=", "作者：").Replace("BestWork=", "作品：").Replace("Born=","誕生日：");
-            var prof = repl.Split(';').ToArray();
-           
-            foreach (var item in prof)
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+          //  var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886"; 
+
+          string[] lines = {
+            "Novelist=谷崎潤一郎;BestWork=春琴妙;Born=1886",
+            "Novelist= 夏目漱石;BestWork=坊ちゃん:Born=1867",
+            "Novelist=太宰治;BestWork=人間失格:Born=1909",
+            "Novelist=宮崎賢治;BestWork=銀河鉄道の夜;Born=1896"
+            };
+            for (int i = 0; i < lines.Length; i++)
             {
-                Console.WriteLine(item);
+                var repl = lines[i].Replace("Novelist=", "作者：").Replace("BestWork=", "作品：").Replace("Born=", "誕生日：");
+                var prof = repl.Split(';').ToArray();
+
+                foreach (var item in prof)
+                {
+
+                    Console.WriteLine(item);
+
+                }
             }
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.ffff")); ;
         }
     }
 }
