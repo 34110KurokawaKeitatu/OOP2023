22d86de6d6b952b6b8c471c487fa3f5cf291728c
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-06-07 11:04:01 +0900

    演習1_1

--- Chapter06/section02(  section01  )/Program.cs
+++ Chapter06/section02(  section01  )/Program.cs
@@ -15,16 +15,25 @@ namespace section01{
 
             //500円以上の本のタイトルをすべて出力
 
-           // Console.WriteLine(books.Average(s => s.Price));
+            // Console.WriteLine(books.Average(s => s.Price));
 
-          //  Console.WriteLine(books.Max(s => s.Price));
+            //  Console.WriteLine(books.Max(s => s.Price));
 
             var Boossoso = books.Where(s => s.Title.Contains("物語")).OrderByDescending(s => s.Price);
+
             Console.WriteLine(Boossoso.Average(s => s.Pages));
+
+            var boo = books.OrderByDescending(s => s.Title.Length);
+
+            foreach(var c in boo)
+            {
+                Console.WriteLine(c.Title);
+            }
 
             foreach (var item in Boossoso)
             {
-                Console.WriteLine("{0}:{1}円",item.Title ,item.Price);
+                Console.WriteLine("{0}:{1}円",item.Title ,item.Price , item.Title);
+              
             }
 
         }
---
+++ Section02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+
+            var number = Enumerable.Range(1, 1000000).ToArray();
+            WriteTotalMemory("配列確保後");
+
+            //偶数だけ取り出す
+            var number1 = Enumerable.Range(1, 1000).Where(s => s % 2 == 0).ToArray();
+            WriteTotalMemory("偶数確保後");
+
+            //平均
+            var avg = number1.Average();
+            WriteTotalMemory("平均確保後");
+
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff")); ;
+        }
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.000 MB}");
+        }
+    }
+}
