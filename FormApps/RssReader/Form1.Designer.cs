83f8697239f3046c5ad383c2a3950ecce31039a1
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-09-28 12:09:18 +0900

    途中

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace RssReader {
-    partial class Form1 {
+    partial class btGet {
         /// <summary>
         /// 必要なデザイナー変数です。
         /// </summary>
@@ -25,13 +25,72 @@ namespace RssReader {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btGetUlr = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(62, 13);
+            this.tbUrl.Multiline = true;
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(623, 53);
+            this.tbUrl.TabIndex = 0;
+            this.tbUrl.TextChanged += new System.EventHandler(this.tbUrl_TextChanged);
+            // 
+            // btGetUlr
+            // 
+            this.btGetUlr.Location = new System.Drawing.Point(691, 15);
+            this.btGetUlr.Name = "btGetUlr";
+            this.btGetUlr.Size = new System.Drawing.Size(72, 51);
+            this.btGetUlr.TabIndex = 1;
+            this.btGetUlr.Text = "取得";
+            this.btGetUlr.UseVisualStyleBackColor = true;
+            this.btGetUlr.Click += new System.EventHandler(this.btGetUlr_Click);
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(62, 91);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(686, 112);
+            this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(62, 210);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.ScriptErrorsSuppressed = true;
+            this.wbBrowser.Size = new System.Drawing.Size(686, 325);
+            this.wbBrowser.TabIndex = 3;
+            // 
+            // btGet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 547);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.btGetUlr);
+            this.Controls.Add(this.tbUrl);
+            this.Name = "btGet";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btGetUlr;
+        private System.Windows.Forms.ListBox lbRssTitle;
+        private System.Windows.Forms.WebBrowser wbBrowser;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,57 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class btGet : Form {
+        public btGet() {
             InitializeComponent();
+        }
+
+        private void tbUrl_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void btGetUlr_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient())
+            {
+                var ulr = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(ulr);
+                var nodes = xdoc.Root.Descendants("item")
+                    .Select(x => new ItemData
+                    {
+                        Title = (string)x.Element("title")
+                    }
+                        );
+
+                foreach (var node in nodes)
+                {
+                    lbRssTitle.Items.Add(node.Title);
+
+                }
+            }
+        }
+
+        private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
+            // wbBrowser.Navigate(lbRssTitle.Items.ToString()); 
+            using (var wc = new WebClient())
+            {
+                var ulr = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(ulr);
+                var nodes = xdoc.Root.Descendants("item")
+                    .Select(x => new ItemData
+                    {
+                     
+                        link = (string)x.Element("link"),
+                    }
+                        );
+            }
         }
     }
 }
