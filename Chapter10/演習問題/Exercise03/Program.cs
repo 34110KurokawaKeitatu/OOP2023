5a8687fa500e970309f12d2a0a789bd8b099f34e
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-10-05 11:21:58 +0900

    演習10-3

--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Exercise03 {
     class Program {
@@ -13,7 +14,15 @@ namespace Exercise03 {
                "It will take time.",
                "We reorganized the timetable.",
             };
-
+            var regex = new Regex(@"\w[T|t]");
+            foreach (var item in texts)
+            {
+                var matchs = Regex.Matches(item,@"\b(T|t)ime\b");
+                    foreach (Match s in matchs)
+                    {
+                    Console.WriteLine("{0} {1}",item,s.Index);
+                    }
+            }
 
 
 
