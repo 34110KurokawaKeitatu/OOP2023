a7ef8aa698f765ae7888a9281f0ad9a810db85aa
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-10-10 11:24:40 +0900

    演習10.5

--- Chapter10/演習問題/Exercise05/Program.cs
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -4,22 +4,27 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Exercise05 {
     class Program {
         static void Main(string[] args) {
             TagLower("sample.html");
+            var text = File.ReadAllText("sample.html");
+            Console.WriteLine(text);
         }
 
         private static void TagLower(string file) {
             var lines = File.ReadLines(file);
             var sb = new StringBuilder();
-
-
-
-
-
-
+            foreach(var line in lines)
+            {
+                var newline = Regex.Replace(line,@"<(/?)([A-Z][A-Z0-9]*)(.*?)>",m => {
+                    return string.Format("<{0}{1}{2}>", m.Groups[1].Value, m.Groups[2].Value.ToLower(), m.Groups[3].Value);
+                }
+                );
+                sb.Append(newline);
+            }
 
             //ファイル出力
             File.WriteAllText(file, sb.ToString());
