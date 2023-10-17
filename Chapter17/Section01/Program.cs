f07888ff5157c2f825c999b9a535ac82beb046cc
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-10-17 10:21:25 +0900

    17章①継承を使ったポリモフィズム

---
+++ Chapter17/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var greetings = new List<GreetingBase>() { new GreetingMorning(),
+                                                       new GreetingAfternoon(),
+                                                       new GreetingEvening(),};
+            foreach (var item in greetings)
+            {
+                string msg = item.GetMessage();
+                Console.WriteLine(msg);
+            }
+        }
+    }
+}
