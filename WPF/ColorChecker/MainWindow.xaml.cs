a53c03202e3ee432dd82d9f0dde3e9fe2ed2d7be
黒川　京達 <OIBC34110@ojs.ac.jp>
2023-11-01 12:05:54 +0900

    ストック

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ColorChecker {
     /// <summary>
@@ -20,6 +13,48 @@ namespace ColorChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            SolidColorBrush scolorBrush = new SolidColorBrush();
+            scolorBrush.Color = Color.FromRgb((byte)redSlider.Value, (byte)greenSlider.Value, (byte)blueSlider.Value);
+            colerArea.Background = scolorBrush;
+        }
+
+        private void stacckBotonn_Click(object sender, RoutedEventArgs e) {
+            string format = String.Format("R{0} G{1} B{2}",redSlider.Value,greenSlider.Value,blueSlider.Value);
+            colorListbox.Items.Add(format);
+            
+        }
+        private void colorListbox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            string[] Click = colorListbox.SelectedItem.ToString().Split(' ');
+            rText.Text = Click[0];
+            gText.Text = Click[1];
+            bText.Text = Click[2];
+        }
+
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+        public class MyColor {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            SolidColorBrush BUr = new SolidColorBrush();
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            BUr.Color = mycolor.Color;
+            redSlider.Value = BUr.Color.R;
+            greenSlider.Value = BUr.Color.G;
+            blueSlider.Value = BUr.Color.B;
+            colerArea.Background = BUr;
+
         }
     }
+
 }
+
+
